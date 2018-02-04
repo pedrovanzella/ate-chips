a92f84a2a4b6564931ee01106a1e20f96bd05937
Pedro Vanzella <pedro@pedrovanzella.com>
2018-02-04 18:47:28 -0200

    Create project

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+((nil .
+      ((cmake-ide-project-dir . "~/Code/C++/ate-chips")
+       (cmake-ide-build-dir . "~/Code/C++/ate-chips/build")
+       (cmake-ide-cmake-opts . "-DCMAKE_BUILD_TYPE=Debug")
+       (helm-make-build-dir . "build")
+       (helm-make-arguments . "-j9")
+       (helm-ctest-dir . "~/Code/C++/ate-chips/build/")
+       )))
