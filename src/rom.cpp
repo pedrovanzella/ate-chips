d7ac564493889ee64d957053b104ac5c8c72fef5
Pedro Vanzella <pedro@pedrovanzella.com>
2018-02-05 15:17:54 -0200

    Add stub implementation of ROM::disassemble_word(int)

--- src/rom.cpp
+++ src/rom.cpp
@@ -24,5 +24,28 @@ uint16_t ROM::get_word(int offset)
 
 std::string ROM::disassemble_word(int offset)
 {
-  return "NOT IMPLEMENTED";
+  auto word = get_word(offset);
+  uint8_t first_nibble = word >> 12; // Opcodes are encoded in the first 4 bits
+
+  switch (first_nibble) {
+  case 0x00:
+  case 0x01:
+  case 0x02:
+  case 0x03:
+  case 0x04:
+  case 0x05:
+  case 0x06:
+  case 0x07:
+  case 0x08:
+  case 0x09:
+  case 0x0a:
+  case 0x0b:
+  case 0x0c:
+  case 0x0d:
+  case 0x0e:
+  case 0x0f:
+    return "NOT IMPLEMENTED";
+  default:
+    return "SOMETHING IS WRONG!";
+  }
 }
--- test/rom_test.cpp
+++ test/rom_test.cpp
@@ -9,7 +9,41 @@ protected:
     std::vector<uint8_t> buff = {0, 1, 2, 3, 4, 5};
     _simple_rom.setBuffer(buff);
 
-    std::vector<uint8_t> cbuff = {};
+    std::vector<uint8_t> cbuff = {0x00, 0xe0, // CLS
+                                  0x00, 0xee, // RET
+                                  0x12, 0x00, // JMP $200
+                                  0x22, 0x00, // CALL $(200)
+                                  0x30, 0x00, // SEQ V0 $00
+                                  0x40, 0x00, // SNE V0 $00
+                                  0x50, 0x10, // SEQ V0 V1
+                                  0x60, 0xab, // MOV V0 $AB
+                                  0x70, 0x02, // ADD V0 $02
+                                  0x80, 0x10, // MOV V0 V1
+                                  0x80, 0x11, // OR V0 V1
+                                  0x80, 0x12, // AND V0 V1
+                                  0x80, 0x13, // XOR V0 V1
+                                  0x80, 0x14, // ADD V0 V1
+                                  0x80, 0x15, // SUB V0 V1
+                                  0x80, 0x16, // SHIFTR V0 V1
+                                  0x80, 0x17, // DIFF V0 V1
+                                  0x80, 0x1e, // SHIFTL V0 V1
+                                  0x90, 0x10, // SNE V0 V1
+                                  0xa2, 0x00, // MOVI $200
+                                  0xb2, 0x00, // JMPO $200
+                                  0xc0, 0xab, // RANDAND V0 $AB
+                                  0xd0, 0x1f, // DRAW V0 V1 $F
+                                  0xe0, 0x9e, // JEQK (V0)
+                                  0xe0, 0xa1, // JNEQK (V0)
+                                  0xf0, 0x07, // MOVT V0
+                                  0xf0, 0x0a, // MOVK V0
+                                  0xf0, 0x15, // MOVT $V0
+                                  0xf0, 0x18, // MOVST $V0
+                                  0xf0, 0x1e, // MOVI $V0
+                                  0xf0, 0x29, // MOVI $(V0)
+                                  0xf0, 0x33, // BCD V0
+                                  0xfa, 0x55, // MOVA $VA
+                                  0xfa, 0x65, // LDA $VA
+    };
     _complex_rom.setBuffer(cbuff);
   }
 
@@ -26,5 +60,13 @@ TEST_F(ROMTest, ReadsWord) {
   EXPECT_EQ(_simple_rom.get_word(0), (0 << 8) + 1);
   EXPECT_EQ(_simple_rom.get_word(1), (2 << 8) + 3);
   EXPECT_EQ(_simple_rom.get_word(2), (4 << 8) + 5);
+}
+
+TEST_F(ROMTest, GetsNibblesRight) {
+  EXPECT_NE(_simple_rom.disassemble_word(0), "SOMETHING IS WRONG!");
+}
+
+TEST_F(ROMTest, DisassemblesEveryOPCode) {
+  
 }
 
